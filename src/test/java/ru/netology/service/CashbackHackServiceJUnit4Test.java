ab8fbc1854699771b821a0fb45346246bfa5c52f
Skineroool <byfender45@gmail.com>
2026-02-15 10:34:53 +0500

    add tests and build gradle for junit4

--- src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
@@ -1,16 +1,16 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
 
-public class CashbackHackServiceTestNG {
+public class CashbackHackServiceJUnit4Test {
 
     @Test
     public void shouldReturn100WhenAmount900() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(900);
         int expected = 100;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
@@ -18,7 +18,7 @@ public class CashbackHackServiceTestNG {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
-        Assert.assertEquals(actual, expected); // Этот упадёт
+        Assert.assertEquals(expected, actual); // Этот упадёт
     }
 
     @Test
@@ -26,7 +26,15 @@ public class CashbackHackServiceTestNG {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(100);
         int expected = 900;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
+
+    @Test
+    public void shouldReturn500WhenAmount1500() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1500);
+        int expected = 500;
+        Assert.assertEquals(expected, actual);
+    }
 }
